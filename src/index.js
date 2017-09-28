7c000689d1b090b7b77823ef68a5a9537ceb85a6
ArtyomLoyko <31865743+ArtyomLoyko@users.noreply.github.com>
2017-09-29 00:29:59 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for(i = 0; i < bracketsConfig.length; i++) {
+    var str1 = bracketsConfig[i].join('');
+    for (j = 0; j < str.length; j++) {
+      if(str.slice(j, j + 2) == str1) {
+        var arr = str.split('');
+        arr.splice(j, 2);
+        str = arr.join('');
+        j = j - 2;
+      }
+    }
+  }
+  if (str !== '') return false;
+  return true;
 }
